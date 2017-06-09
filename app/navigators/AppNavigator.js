198d242ff682eaab15f7ad18715ee0a1d375348b
abeeralshaer <abber2000g@gmail.com>
2017-06-09 22:36:17 +0300

    add tab navigator

--- app/navigators/AppNavigator.js
+++ app/navigators/AppNavigator.js
@@ -2,15 +2,45 @@ import Splash from '../components/SplashScreen.js';
 import Main from '../components/MainScreen.js';
 import Login from '../components/LoginScreen.js';
 import Signup from '../components/SignupScreen.js';
+import Subscribe from '../components/SubscribeScreen.js';
+import ShoppingList from '../components/ShoppingListScreen.js';
+import MyPlan from '../components/MyPlan.js';
+import DailyCalculator from '../components/DailyCalculatorScreen.js';
+import AskExpert from '../components/AskExpertScreen.js';
+
+
 import {StackNavigator,TabNavigator} from 'react-navigation';
 
+const MainTabs = {
+  Main: {
+    screen: Main,
+    navigationOptions: {
+      tabBarLabel: 'الرئيسية'
+  }
+
+  },
+  DailyCalculator: {
+    screen: DailyCalculator,
+    navigationOptions: {
+      tabBarLabel: 'حاسبةالسعرات'
+  }
+  },
+  AskExpert: {
+    screen: AskExpert,
+    navigationOptions: {
+      tabBarLabel: 'إسأل الخبير'
+  }
+}
+};
+
+const MainTabsNavigator = TabNavigator(MainTabs);
 
 const Routes = {
   Splash: {
     screen: Splash
   },
-  Main: {
-    screen: Main
+  Home: {
+    screen: MainTabsNavigator
   },
   Login : {
     screen: Login
