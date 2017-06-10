6a44591e9d5aead9cc08905a0f283cffab58b293
abeeralshaer <abber2000g@gmail.com>
2017-06-10 03:54:46 +0300

    add new icon to screen

--- app/components/MainScreen.js
+++ app/components/MainScreen.js
@@ -3,12 +3,21 @@ import {
   View,
   Text
 } from 'react-native';
+import {Icon} from 'react-native-elements';
 
 const MainScreen = ()=>{
   return(
 
     <View>
     <Text>MainScreen</Text>
+      <Icon
+      raised
+  name='heartbeat'
+  type='font-awesome'
+  color='#f50'
+        onPress={() => {}}
+
+      />
     </View>
 
   );
