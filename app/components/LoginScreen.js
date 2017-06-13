73a01033c2085dc12f170272a85b0c20b91faba2
abeeralshaer <abber2000g@gmail.com>
2017-06-13 16:15:30 +0300

    change login to statefull

--- app/components/LoginScreen.js
+++ app/components/LoginScreen.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {
   View,
   Text,
-  Button
+  Button,
+  TextInput,
+  StyleSheet
 } from 'react-native';
 
-const LoginScreen = ({navigation})=>{
-  return(
+class LoginScreen extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      email:'',
+      password:''
+    };
+  }
 
-    <View>
-    <Text>LoginScreen</Text>
+
+
+  render() {
+    const{navigation}=this.props;
+    return(
+
+    <View   style={{
+      flex: 1,
+      alignSelf: 'stretch',
+      width: undefined,
+      height: undefined
+    }}>
+    <Text style={styles.text}>اسم المستخدم أو الايميل</Text>
+    <TextInput
+       style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+       onChangeText={(text) => this.setState({text})}
+       value={this.state.email}
+     />
+     <Text style={styles.text}>كلمة المرور</Text>
+     <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.password}
+      />
     <Button title='تسجيل دخول' onPress={()=>navigation.navigate('LaunchApp')}></Button>
     </View>
 
-  );
-};
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'thesans'
+  }
+});
 
 export default LoginScreen;
