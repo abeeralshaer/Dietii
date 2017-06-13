ce9669d5198ec6ea850d314df131ae5f2b9de285
abeeralshaer <abber2000g@gmail.com>
2017-06-13 16:15:42 +0300

    delete some code

--- app/components/MainScreen.js
+++ app/components/MainScreen.js
@@ -3,21 +3,14 @@ import {
   View,
   Text
 } from 'react-native';
-import {Icon} from 'react-native-elements';
+
 
 const MainScreen = ()=>{
   return(
 
     <View>
     <Text>MainScreen</Text>
-      <Icon
-      raised
-  name='heartbeat'
-  type='font-awesome'
-  color='#f50'
-        onPress={() => {}}
 
-      />
     </View>
 
   );
