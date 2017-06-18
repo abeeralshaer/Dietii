36f0c240a21540b5b35116c9f469bf3a6c376ebd
abeeralshaer <abber2000g@gmail.com>
2017-06-18 17:37:23 +0300

    delete store and middleware

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,29 @@
 import {combineReducers} from 'redux';
 import * as userReducers from './user.js';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger';
 
-export default combineReducers(
+
+
+const reducers = combineReducers(
   {
     user : userReducers,
     nav  :userReducers.navReducer
 
-  }
-);
+  });
+const loggerMiddleware = createLogger({predicate: (getState,action) => __DEV__});
+
+const configureStore = (initialState)=> {
+  const enhancer = compose(
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware
+    )
+  );
+  return createStore(reducers,initialState,enhancer);
+};
+
+const store = configureStore({});
+
+export default store;
