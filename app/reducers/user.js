909906c1ad4f01e366cd02299dce94ab76fbc5a2
abeeralshaer <abber2000g@gmail.com>
2017-06-18 17:37:52 +0300

    add DrawerIcon component

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -1,24 +1,11 @@
-import * as types from '../actions/types.js';
 import {OpenAppStack} from '../navigators/AppNavigator.js';
 
-export const login = (state = {},action)=>{
-  switch (action.type) {
-  case types.LOGIN:
-    return Object.assign({},state,{
-      isLoggedin : true
-    });
 
-  default:
-
-  }
-};
 
 const initialAction = OpenAppStack.router.getActionForPathAndParams('Splash');
-
 const initialState = OpenAppStack.router.getStateForAction(initialAction);
-
 export const navReducer = (state = initialState ,action)=>{
-
+  const {type, routeName} = action;
   const nextState = OpenAppStack.router.getStateForAction(action,initialState);
-  return  nextState || state;
+    return  nextState || state;
 };
--- index.android.js
+++ index.android.js
@@ -3,29 +3,8 @@ import {
   AppRegistry,
 } from 'react-native';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import {createLogger} from 'redux-logger';
-import reducer from './app/reducers/index.js';
 import App from './app/containers/App.js';
-
-
-
-const loggerMiddleware = createLogger({predicate: (getState,action) => __DEV__});
-
-const configureStore = (initialState)=> {
-  const enhancer = compose(
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
-    )
-  );
-  return createStore(reducer,initialState,enhancer);
-};
-
-const store = configureStore({});
-
-
+import store from './app/reducers/index.js'
 
 
 export const Dietii = () => {
